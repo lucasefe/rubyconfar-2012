5a0a09014cc5f653fee1a484a5461de9d8291dbe
Nico Sztabzyb <nico@szstudios.net>
2012-05-04 17:13:45 -0300

    Starting to lay things out a bit.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,3 +7,14 @@ $(function() {
     return false
   })
 })
+
+
+$(document).ready(function(){
+  $("img.a").hover(
+    function() {
+    $(this).stop().animate({"opacity": "0"}, "slow");
+    },
+    function() {
+    $(this).stop().animate({"opacity": "1"}, "slow");
+  });
+});
