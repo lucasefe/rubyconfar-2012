093c81af2f473a68ded5c3287984426aeb22cd3e
Lucas Florio <lucasefe@gmail.com>
2012-08-10 11:34:43 -0300

    Adding very basic speakers page, so Nico can start working.

--- db/seed.rb
+++ db/seed.rb
@@ -15,7 +15,11 @@ libre toca música e intenta practicar deportes (sin mucho éxito)."
 
 jano.save
 
-pedro = Speaker.create first_name: "Pedro", last_name: "Belo", twitter: "pedro"
-jano = Speaker.create first_name: "Nicolás", last_name: "Sanguinetti", twitter: "godfoca"
-apotonick = Speaker.create first_name: "Nick", last_name: "Suterer", twitter: "apotonick"
-wynn = Speaker.create first_name: "Wynn", last_name: "Netherland", twitter: 'pengwynn'
+pedro = Speaker.create first_name: "Pedro", last_name: "Belo",
+  twitter: "pedro", company: "Heroku"
+jano = Speaker.create first_name: "Nicolás", last_name: "Sanguinetti",
+  twitter: "godfoca"
+apotonick = Speaker.create first_name: "Nick", last_name: "Suterer",
+  twitter: "apotonick"
+wynn = Speaker.create first_name: "Wynn", last_name: "Netherland",
+  twitter: 'pengwynn', company: "Github"
--- models/speaker.rb
+++ models/speaker.rb
@@ -1,3 +1,6 @@
 class Speaker < Sequel::Model
 
+  def display_name
+    [first_name, last_name].join(" ")
+  end
 end
