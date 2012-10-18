63453cdcdab7af339354d8552e55615a3a2ef16e
Nico Sztabzyb <nico@szstudios.net>
2012-10-18 20:58:27 -0300

    Making the Event page happen.

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -1,5 +1,5 @@
 $(function(){
-  flickrFeed("#photos");
+  flickrFeed("#photos .slides_container");
   twitterFeed("#tweets");
   slideIt("#photos .slides_container");
   slideIt("#announcements");
@@ -7,8 +7,8 @@ $(function(){
 
 function twitterFeed(container) {
   $(container).tweet({
-    avatar_size: 32,
-    count: 10,
+    avatar_size: 40,
+    count: 7,
     query: "#RubyConfAr",
     loading_text: "searching twitter..."
   });
@@ -16,10 +16,11 @@ function twitterFeed(container) {
 
 function flickrFeed(container) {
   var url = "http://api.flickr.com/services/feeds/groups_pool.gne?id=2068965@N25&format=json&jsoncallback=?"
-  var anstring = String.fromCharCode(38);
   $.getJSON(url, function(data){
     $.each(data.items, function(i,item){
-      $("<img/>").attr("src", item.media.m).appendTo(container).wrap("<div></div>");
+      $("<img/>").attr("src", item.media.m)
+        .appendTo(container)
+        .wrap("<div><a href='" + item.link + "'></a></div>");
     });
   });
 }
